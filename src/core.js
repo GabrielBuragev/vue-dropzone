09acf80eabf7351f3cad23ef339088e23fdd8020
GabrielBuragev <gabriel.buragev96@gmail.com>
2020-01-29 12:24:18 +0100

    Fix reference to undefined function

--- src/core.js
+++ src/core.js
@@ -1,4 +1,3 @@
-import Vue from "vue";
 import { SimpleDropzone } from "simple-dropzone";
 import CustomDropzone from "./lib/dropzone";
 
@@ -7,49 +6,51 @@ let DropController;
 let binded = false;
 let lastActiveFlag = null;
 
-let onBind = (el, binding, vnode) => {
-    if (!binding.value.active) {
-        lastActiveFlag = binding.value.active;
-        return;
-    }
-    DropController = new SimpleDropzone(el, document.createElement("input"));
-    Dropzone = new CustomDropzone(binding.value ? binding.value : {});
+const onBind = (el, binding, vnode) => {
+  if (!binding.value.active) {
+    lastActiveFlag = binding.value.active;
+    return;
+  }
+  DropController = new SimpleDropzone(el, document.createElement("input"));
+  Dropzone = new CustomDropzone(binding.value ? binding.value : {});
 
-    DropController.on("drop", Dropzone.onDrop);
-    DropController.on("dropstart", Dropzone.onDropStart);
-    DropController.on("droperror", Dropzone.onDropError);
-    el.addEventListener("dragenter", Dropzone.onDragEnter);
-    el.addEventListener("dragleave", Dropzone.onDragLeave);
+  DropController.on("drop", Dropzone.onDrop);
+  DropController.on("dropstart", Dropzone.onDropStart);
+  DropController.on("droperror", Dropzone.onDropError);
+  el.addEventListener("dragenter", Dropzone.onDragEnter);
+  el.addEventListener("dragleave", Dropzone.onDragLeave);
 
-    binded = true;
+  binded = true;
 };
-let onDestroy = (el, binding, vnode) => {
+const onDestroy = (el, binding, vnode) => {
+  if (DropController) {
     el.removeEventListener("dragover", DropController._onDragover);
     el.removeEventListener("drop", DropController._onDrop);
     el.removeEventListener("dragenter", Dropzone.onDragEnter);
     el.removeEventListener("dragleave", Dropzone.onDragLeave);
     binded = false;
+  }
 };
-let onUpdate = (el, binding, vnode) => {
-    if (binding.value.active === lastActiveFlag) return;
+const onUpdate = (el, binding, vnode) => {
+  if (binding.value.active === lastActiveFlag) return;
 
-    if (!binding.value.active && binded) {
-        try {
-            onDestroy(el, binding, vnode);
-        } catch (e) {
-            console.log(e);
-        }
-    } else if (binding.value.active && !binded) {
-        try {
-            onBind(el, binding, vnode);
-        } catch (e) {
-            console.log(e);
-        }
+  if (!binding.value.active && binded) {
+    try {
+      onDestroy(el, binding, vnode);
+    } catch (e) {
+      console.log(e);
     }
-    lastActiveFlag = binding.value.active;
+  } else if (binding.value.active && !binded) {
+    try {
+      onBind(el, binding, vnode);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  lastActiveFlag = binding.value.active;
 };
 export default {
-    bind: onBind,
-    unbind: onDestroy,
-    update: onUpdate
+  bind: onBind,
+  unbind: onDestroy,
+  update: onUpdate
 };
